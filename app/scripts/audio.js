f6323217d6f6cdf4fb4e045e7e25a422c14c1eb3
Travis Rasor <travis.rasor@argodata.com>
2016-08-23 17:06:32 -0500

    Adding breakout node class and spec

--- app/scripts/audio.js
+++ app/scripts/audio.js
@@ -1,5 +1,87 @@
 
+class AudioContextBean {
 
-function doWhat() {
-  return false;
+  constructor() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    this._context = new AudioContext();
+  }
+
+  get context() {
+    return this._context
+  }
+
+  close() {
+    this._context.close()
+  }
 }
+
+class BreakoutNode {
+
+  constructor(node, context) {
+    this._node = node
+    this._outputNodes = []
+    this._gain = context.createGain()
+
+    this._node.connect(this._gain);
+  }
+
+  get node() {
+    return this._node;
+  }
+
+  isConnected(outNode) {
+    //if already connected, ignore
+    if (this._outputNodes.indexOf(outNode) > -1) {
+      console.log("node already connected!" + outNode);
+      return true;
+    }
+    return false;
+  }
+
+  connect(outNode){
+    if (this.isConnected(outNode)){
+      return;
+    }
+    this._outputNodes.push(outNode);
+
+    this._reconnectNodes()
+  }
+
+  disconnect(outNode) {
+    let position = this._outputNodes.indexOf(outNode);
+
+    //ignore if outNode not connected
+    if (position<0) {
+      console.log('node was not connected!' + outNode);
+      return;
+    }
+
+    this._outputNodes.splice(position, 1);
+
+    this._reconnectNodes()
+  }
+
+  _reconnectNodes() {
+    let i
+    this._gain.disconnect();
+    for(i=0; i < this._outputNodes.length; i++) {
+      this._gain.connect(this._outputNodes[i]);
+    }
+  }
+
+  disconnectAll() {
+    this._outputNodes = []
+    this._gain.disconnect()
+  }
+
+}
+
+var OscillatorBreakoutNode = function() {
+    osc = new BreakoutNode(context.createOscillator(), context)
+
+    osc.node.type = osc.node.SAWTOOTH;
+    osc.node.frequency.value = 100;
+    osc.node.start(0);
+}
+
+
--- test/spec/audio.js
+++ test/spec/audio.js
@@ -1,10 +1,103 @@
 (function () {
   'use strict';
 
-  describe('AudioStuffIsLoaded', function () {
-    describe('Functions can be called', function () {
-      it('this should be called and false', function () {
-          assert.equal(doWhat(), false)
+  describe('AudioContextBean', function () {
+    let ctx = new AudioContextBean()
+
+    describe('Contains a working audio context object', function () {
+      it('Can be accessed through a getter', function () {
+          expect(ctx).to.exist;
+          expect(ctx).to.have.property('context')
+      });
+
+      it('Can construct nodes and things', function () {
+          let gain = ctx.context.createGain()
+          expect(gain).to.exist;
+          expect(gain).to.have.property('gain')
+      });
+
+      it('is Closeable', function () {
+          let ctx1 = new AudioContextBean()
+          ctx1.close()
+          expect(ctx1.context).to.have.property("state","closed")
+      });
+    });
+  });
+
+  describe('BreakoutNode', function () {
+
+    let ctx = new AudioContextBean();
+    let osc = ctx.context.createOscillator();
+
+    describe('Encapsulates an audioNode', function () {
+      it('Can be accessed through a getter', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          expect(breakout).to.exist;
+          expect(breakout).to.have.property("node").that.equals(osc);
+      });
+      it('is connected to only the gain node', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          expect(breakout).to.have.property("node")
+            .that.has.property("numberOfOutputs").that.equals(1);
+          expect(breakout).to.have.property("node")
+            .that.has.property("numberOfInputs").that.equals(0);
+      });
+    });
+
+    describe('Contains a gain node for output control', function () {
+      it ('Has one input from the audio node', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          expect(breakout).to.exist;
+          expect(breakout).to.have.property("_gain")
+            .that.has.property("numberOfInputs").that.equals(1);
+      });
+    });
+    describe('Encapsulates output connections from the node', function () {
+
+      it('Can add output connections to other audio nodes', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          let osc1 = ctx.context.createGain();
+          let osc2 = ctx.context.createGain();
+
+          breakout.connect(osc1);
+          expect(breakout._outputNodes).to.have.lengthOf(1)
+          expect(breakout.isConnected(osc1)).to.be.true;
+
+          breakout.connect(osc2);
+          expect(breakout._outputNodes).to.have.lengthOf(2)
+          expect(breakout.isConnected(osc2)).to.be.true;
+      });
+
+      it('Can disconnect individual output connections to other audio nodes', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          let osc1 = ctx.context.createGain();
+          let osc2 = ctx.context.createGain();
+
+          breakout.connect(osc1);
+          breakout.connect(osc2);
+
+          breakout.disconnect(osc1);
+          expect(breakout._outputNodes).to.have.lengthOf(1)
+          expect(breakout.isConnected(osc1)).to.be.false;
+          expect(breakout.isConnected(osc2)).to.be.true;
+
+          breakout.disconnect(osc2);
+          expect(breakout._outputNodes).to.be.empty;
+          expect(breakout.isConnected(osc1)).to.be.false;
+          expect(breakout.isConnected(osc2)).to.be.false;
+      });
+
+      it('Can disconnect all output connections to other audio nodes', function () {
+          let breakout = new BreakoutNode(osc, ctx.context)
+          let osc1 = ctx.context.createGain();
+          let osc2 = ctx.context.createGain();
+
+          breakout.connect(osc1);
+          breakout.connect(osc2);
+          breakout.disconnectAll();
+          expect(breakout._outputNodes).to.be.empty;
+          expect(breakout.isConnected(osc1)).to.be.false;
+          expect(breakout.isConnected(osc2)).to.be.false;
       });
     });
   });
